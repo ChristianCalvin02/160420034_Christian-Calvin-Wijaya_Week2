f0969f0504b5529c0b452011dee0e9631e2d1286
ChristianCalvin02 <christian.calvinwijaya02@gmail.com>
2023-03-14 16:58:21 +0700

    Update File Week 3

--- app/src/main/java/com/example/adv160420034week2/MainActivity.kt
+++ app/src/main/java/com/example/adv160420034week2/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -16,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         navController = (supportFragmentManager.findFragmentById(R.id.hostFragment)as
                 NavHostFragment).navController
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
